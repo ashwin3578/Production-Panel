acc42958bb3d2e79c61eaa0199a89426fac8d949
Cocololco <corentinhillion@gmail.com>
2023-03-13 12:12:25 +1000

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php 
-$page_title='Template Page';
-$title_top='Template Page';
+$page_title='Test Page';
+$title_top='Test Page';
 $page = $_SERVER['PHP_SELF'];
 ?>
 
@@ -18,6 +18,68 @@ $page = $_SERVER['PHP_SELF'];
     <div class="col-xs-6"></div>
     <div class="col-xs-6">
     </div>
-    <?php send_email('production-assistant@sicame.com.au',"Test","content email 123","Test Email",$cc='');?>
+    <?php 
+    use PHPMailer\PHPMailer\PHPMailer;
+    $address="production-assistant@sicame.com.au";
+    $name="Test";
+    $content="content email 123";
+    $subject="Test Email";
+    //send_email('production-assistant@sicame.com.au',"Test","content email 123","Test Email",$cc='');
+    require 'composer/vendor/phpmailer/phpmailer/src/Exception.php';
+    require 'composer/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+        
+    //Load Composer's autoloader
+    require 'composer/vendor/autoload.php';
+    
+
+    //Instantiation and passing `true` enables exceptions
+
+        //Create a new PHPMailer instance
+        $mail = new PHPMailer();
+        //Server settings
+
+        if($_SERVER['PHP_SELF']=='/test.php')      {$mail->SMTPDebug = 3;} 
+        //$mail->SMTPDebug = 3;                    //Enable verbose debug output SMTP::DEBUG_SERVER
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'mail.sicame.com.au';             //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = get_setting('email_production_assistant');  //SMTP username
+        $mail->Password   = get_setting('password_production_assistant');   //SMTP password
+        $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 587;   		//TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $mail->SMTPOptions = array(
+        'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );   
+        
+        //Recipients
+        $mail->setFrom(get_setting('email_production_assistant'), 'Production Assistant');
+        $mail->addAddress($address, $name);     //Add a recipient
+        
+        //CCs
+        if($cc<>''){
+            $allcc=explode(";",$cc);
+            foreach ($allcc as &$onecc){
+                $mail->addCC($onecc);
+            }	
+        }
+        
+        //Content
+        $mail->isHTML(true);  //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $content;
+        $mail->AltBody = $content;
+            //show($mail);
+        // send the message, check for errors
+        if (!$mail->send()) {
+        	echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+        	echo 'Message sent!';
+        }
+    
+    ?>
 </div>
 
