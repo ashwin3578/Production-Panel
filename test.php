532be7c78324c346a2081b37f4f71f936db33501
Cocololco <116242938+Cocololco@users.noreply.github.com>
2023-04-17 19:26:18 -0400

    Update test.php

--- test.php
+++ test.php
@@ -20,66 +20,11 @@ $page = $_SERVER['PHP_SELF'];
     </div>
     <?php 
     use PHPMailer\PHPMailer\PHPMailer;
-    $address="production-assistant@sicame.com.au";
+    $address="corentinhillion@gmail.com";
     $name="Test";
     $content="content test email";
     $subject="Test Email";
-    //send_email('production-assistant@sicame.com.au',"Test","content email 123","Test Email",$cc='');
-    require 'composer/vendor/phpmailer/phpmailer/src/Exception.php';
-    require 'composer/vendor/phpmailer/phpmailer/src/PHPMailer.php';
-        
-    //Load Composer's autoloader
-    require 'composer/vendor/autoload.php';
-    
-
-    //Instantiation and passing `true` enables exceptions
-
-        //Create a new PHPMailer instance
-        $mail = new PHPMailer();
-        //Server settings
-
-        if($_SERVER['PHP_SELF']=='/test.php')      {$mail->SMTPDebug = 3;} 
-        //$mail->SMTPDebug = 3;                    //Enable verbose debug output SMTP::DEBUG_SERVER
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = '172.31.28.30';             //mail.sicame.com.au Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = get_setting('email_production_assistant');  //SMTP username
-        $mail->Password   = get_setting('password_production_assistant');   //SMTP password
-        $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;   		//TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-        $mail->SMTPOptions = array(
-        'ssl' => array(
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-                'allow_self_signed' => true
-            )
-        );   
-        
-        //Recipients
-        $mail->setFrom(get_setting('email_production_assistant'), 'Production Assistant');
-        $mail->addAddress($address, $name);     //Add a recipient
-        
-        //CCs
-        if($cc<>''){
-            $allcc=explode(";",$cc);
-            foreach ($allcc as &$onecc){
-                $mail->addCC($onecc);
-            }	
-        }
-        
-        //Content
-        $mail->isHTML(true);  //Set email format to HTML
-        $mail->Subject = $subject;
-        $mail->Body    = $content;
-        $mail->AltBody = $content;
-            //show($mail);
-        // send the message, check for errors
-        if (!$mail->send()) {
-        	echo 'Mailer Error: ' . $mail->ErrorInfo;
-        } else {
-        	echo 'Message sent!';
-        }
-    
+    send_email('production-assistant@sicame.com.au',"Test","content email 123","Test Email",$cc='');    
     ?>
 </div>
 
